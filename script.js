01d3ec4fa621b7b8e0ebbd381c12f9eb9b1bb0f9
jeslaa <jesperlaaksonen@hotmail.com>
2022-10-11 17:08:18 +0200

    Jobbar på email validering

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const firstName = document.getElementById("firstName")
 const lastName = document.getElementById("lastName")
-const email = document.getElementById("email")
+const email = document.getElementById("email").value
 const password = document.getElementById("password")
 const repeatPassword = document.getElementById("repeatPassword")
 const checkbox = document.getElementById("checkbox")
@@ -14,15 +14,24 @@ form.addEventListener('submit', (e) => {
     if(firstName.value === ""){
         console.log("First name is required");
         messages.push("First name is required")
+    }
     
-    if (firstName.value.length <= 1){
-        console.log("First name cannot be less than 2 charachters");
-        messages.push("First name cannot be less than 2 charachters")
+    else if (firstName.value.length <= 1){
+        console.log("First name cannot be less than 2 characters");
+        messages.push("First name cannot be less than 2 characters")
     }
     
     if(lastName.value === ""){
         console.log("Last name is required");
         messages.push("Last name is required")
+    }
+    //email validation fungerar inte just nu
+    if (email.match(regEx)){
+        console.log("Email is valid");
+    }
+    else if(email.match(regEx == "")){
+        console.log("Email is invalid");
+        messages.push("Email is invalid")
     }
 
     if(password.value.length <= 6){
@@ -38,18 +47,18 @@ form.addEventListener('submit', (e) => {
         console.log("Passwords are matching");
     }
 
-    // if(checkbox.value != checkbox.value.checked){
-    //     console.log("You need to check the checkbox");
-    //     messages.push("You need to check the checkbox")
-    // }
-    // else if(checkbox.value === checkbox.value.checked){
-    //     console.log("checkbox is checked");
-    // }
+    if(!this.form.checkbox.checked){
+        console.log("You need to check the checkbox");
+        messages.push("You need to check the checkbox")
+       
+    }
+
     
-    // if(messages.length > 0){
-    //     e.preventDefault()
-    // }
+     
+    if(messages.length > 0){
+         e.preventDefault()
+     }
     e.preventDefault()
-}
+
     
 })
